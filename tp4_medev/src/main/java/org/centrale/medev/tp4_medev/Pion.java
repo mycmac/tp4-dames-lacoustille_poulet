d543e6f6038b6ea981e857e2e172587299afba4e
Corentin-Poulet <corentin.poulet@eleves.ec-nantes.fr>
2024-12-05 11:47:19 +0100

    Ajout de la méthode déplacement pour un pion

--- tp4_medev/src/main/java/org/centrale/medev/tp4_medev/Pion.java
+++ tp4_medev/src/main/java/org/centrale/medev/tp4_medev/Pion.java
@@ -11,5 +11,60 @@ package org.centrale.medev.tp4_medev;
 public class Pion {
     private Point2D pos;
     private int couleur;
+
+    public Point2D getPos() {
+        return pos;
+    }
+
+    public int getCouleur() {
+        return couleur;
+    }
+
+    public void setPos(Point2D pos) {
+        this.pos = pos;
+    }
+
+    public void setCouleur(int couleur) {
+        this.couleur = couleur;
+    }
+     
+    public void deplace(Plateau plateau,int n){
+        switch (n){
+            case 0: 
+                if(plateau.getContenu()[this.pos.getX()][this.pos.getY()] instanceof Pion){
+                    if(this.couleur==this.BLANC){
+                        this.setPos(new Point2D(this.pos.getX()-1,this.pos.getY()+2));
+                    }
+                    else{
+                        this.setPos(new Point2D(this.pos.getX()+1,this.pos.getY()+2));
+                    }
+                }
+                else{
+                    if(this.couleur==this.BLANC){
+                        this.setPos(new Point2D(this.pos.getX()-1,this.pos.getY()+1));
+                    }
+                    else{
+                        this.setPos(new Point2D(this.pos.getX()+1,this.pos.getY()+1));
+                    }
+                }
+            case 1:
+                if(plateau.getContenu()[this.pos.getX()][this.pos.getY()] instanceof Pion){
+                    if(this.couleur==this.BLANC){
+                        this.setPos(new Point2D(this.pos.getX()-1,this.pos.getY()-2));
+                    }
+                    else{
+                        this.setPos(new Point2D(this.pos.getX()+1,this.pos.getY()-2));
+                    }
+                }
+                else{
+                    if(this.couleur==this.BLANC){
+                        this.setPos(new Point2D(this.pos.getX()-1,this.pos.getY()-1));
+                    }
+                    else{
+                        this.setPos(new Point2D(this.pos.getX()+1,this.pos.getY()-1));
+                    }  
+                }           
+        }
+    }
     
 }
